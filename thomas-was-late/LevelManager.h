a4a4368ada18eff509746a89eb5db0754f198762
Gordon Stirling <gordon.stirling@ayrshire.ac.uk>
2021-12-07 10:12:51 +0000

    Created Level Manager class and added test level text files

---
+++ thomas-was-late/LevelManager.h
@@ -0,0 +1,41 @@
+#ifndef LEVEL_MANAGER_H
+#define LEVEL_MANAGER_H
+
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+
+#include <sstream>
+#include <fstream>
+
+using namespace sf;
+using namespace std;
+
+class LevelManager {
+
+private:
+	Vector2i m_LevelSize;
+	Vector2f m_StartPosition;
+
+	float m_TimeModifier = 1;
+	float m_BaseTimeLeft = 0;
+	
+	int m_CurrentLevel = 0;
+	
+	const int NUM_LEVELS = 4;
+public:
+	const int TILE_SIZE = 50;
+	const int VERTS_IN_QUAD = 4;
+
+	float getTimeLimit();
+
+	Vector2f getStartPosition();
+
+	int** nextLevel(VertexArray& rVaLevel);
+
+	Vector2i getLevelSize();
+
+	int getCurrentLevel();
+};
+
+
+#endif LEVEL_MANAGER_H
